d1a0808477242de47244b0ef62cd91386f455972
Sanil Doshi <sdoshi@chapman.edu>
2023-10-17 14:13:30 -0700

    updated some syntax eerrors within the design-blocks folder

--- milestone1/design-blocks/js/simulated_backend.js
+++ milestone1/design-blocks/js/simulated_backend.js
@@ -14,3 +14,12 @@
  function schedule_device_activation(device, startTime, endTime) {
    console.log(`Scheduled ${device} to turn on at ${startTime} and off at ${endTime}`);
  }
+
+ // Simulated Backend Functions for Location-Based Activation
+function location_based_activation(device, distance_threshold) {
+  if (user_location <= distance_threshold) {
+    console.log(`User is within ${distance_threshold} miles. Turning on ${device}.`);
+  } else {
+    console.log(`User is farther than ${distance_threshold} miles. Turning off ${device}.`);
+  }
+}
